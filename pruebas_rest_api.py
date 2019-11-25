aad9f31643ffbf5d12fac4bfa539f8d9312a6e41
Mauries <mespejovaras@gmail.com>
2019-11-25 21:08:12 +0100

    pruebas_rest_api

--- pruebas_rest_api.py
+++ pruebas_rest_api.py
@@ -1,61 +1,77 @@
 import requests
 import json
 
-URL = "http://127.0.0.1:8000/api/articulos/"
 
-titulo='Titulo3'
-
-PARAMS = {'titulo':titulo}
-
-r = requests.get(url = URL, params = PARAMS)
-
-data = r.json()
-
-print(data)
+def getArticulos(URL):
+    response = requests.get(url=URL)
+    if response.status_code == 200:
+        data = response.json()
+        print(data)
+    else:
+        print('Error getArticulos')
 
 
-r= requests.post('http://httpbin.org/post', json={"key": "value"})
-r.status_code
-
-print(r)
-print(r.json())
-
-
-
-
-
-data = {"articulo":{
-            "titulo": "TituloNuevo",
-            "contenido": "Contenido NUEVO",
-            "autores_id": 1
-}}
+def createArticulo(URL, data):
+    response = requests.post(URL, json=data, headers=headers)
+    if response.status_code == 200:
+        text_url = response.text
+        print(text_url)
+    else:
+        print('Error createArticulo')
 
 
-#headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
-headers = {'Accept':'application/json, text/plain', 'Content-type': 'application/json',}
-r = requests.post(URL, json=data, headers=headers)
-pastbin_url = r.text
-print(pastbin_url)
+def updateArticulo(URL, data, pk):
+    URL = URL+str(pk)
+    response = requests.put(URL, json=data, headers=headers)
+    if response.status_code == 200:
+        pastbin_url = response.text
+        print(pastbin_url)
+    else:
+        print('Error updateArticulo')
 
 
-
-URL = "http://127.0.0.1:8000/api/articulos/17"
-r = requests.delete(URL, headers=headers)
-pastbin_url = r.text
-print(pastbin_url)
-
-
-data = {"articulo":{
-            "titulo": "TituloNuevoModificado",
-            "contenido": "Contenido NUEVOModificado",
-            "autores_id": 1
-}}
+def deleteArticulo(URL, pk):
+    URL = URL+str(pk)
+    response = requests.delete(URL, headers=headers)
+    if response.status_code == 200:
+        pastbin_url = response.text
+        print(pastbin_url)
+    else:
+        pastbin_url = response.text
+        print(pastbin_url)
 
 
+if __name__ == "__main__":
 
+    # headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+    URL = "http://127.0.0.1:8000/api/articulos/"
+    headers = {'Accept': 'application/json, text/plain', 'Content-type': 'application/json'}
+    print("\nARTICULOS: ")
+    getArticulos(URL)
 
-URL = "http://127.0.0.1:8000/api/articulos/19"
-headers = {'Accept':'application/json, text/plain', 'Content-type': 'application/json',}
-r = requests.put(URL, json=data, headers=headers)
-pastbin_url = r.text
-print(pastbin_url)
+    data = {"articulo": {
+                "id": 8,
+                "titulo": "Titulo Nuevo",
+                "contenido": "Contenido Nuevo",
+                "autores_id": 1}}
+
+    print("\nCreando articulo con id 8:")
+    createArticulo(URL, data)
+    getArticulos(URL)
+    print("\n")
+
+    print("Modificando articulo con id 8:")
+    pk = 8
+    data = {"articulo": {
+                "titulo": "TituloModificado",
+                "contenido": "Contenido Modificado",
+                "autores_id": 1}}
+    updateArticulo(URL, data, pk)
+    getArticulos(URL)
+    # for i in range(12, 21:
+    # deleteArticulo(URL, i)
+    print("\n")
+    print("Eliminando articulo con id 8:")
+    pk = 8
+    deleteArticulo(URL, pk)
+    getArticulos(URL)
